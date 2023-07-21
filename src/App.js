2d63bf9f5d497476f042a6397c24bd0c5dce0f94
akeros <mixali4kin@bk.ru>
2023-07-21 22:32:56 +0400

    pre finish

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-// import witcher from "./screen-1.png";
-// import witcherBack from "./image/screen-1/layer-back.jpg";
-// import witcherMiddle from "./image/screen-1/layer-middle.png";
-// import witcherFront from "./image/screen-1/layer-front.png";
-//
-// import geraBack from "./image/screen-2/layer-back.jpg";
-// import geraFront from "./image/screen-2/layer-front.png";
-// import gera from "./image/screen-2/layer-gera.png";
-// import yen from "./image/screen-2/layer-yen.png";
-//
-// import ciriBack from "./image/screen-3/layer-back.jpg";
-// import ciriMiddle from "./image/screen-3/layer-middle.png";
-// import ciriFront from "./image/screen-3/layer-front.png";
-// import ciri from "./image/screen-3/layer-ciri.png";
+import witcher from "./screen-1.png";
+import witcherBack from "./image/screen-1/layer-back.jpg";
+import witcherMiddle from "./image/screen-1/layer-middle.png";
+import witcherFront from "./image/screen-1/layer-front.png";
+
+import geraBack from "./image/screen-2/layer-back.jpg";
+import geraFront from "./image/screen-2/layer-front.png";
+import gera from "./image/screen-2/layer-gera.png";
+import yen from "./image/screen-2/layer-yen.png";
+
+import ciriBack from "./image/screen-3/layer-back.jpg";
+import ciriMiddle from "./image/screen-3/layer-middle.png";
+import ciriFront from "./image/screen-3/layer-front.png";
+import ciri from "./image/screen-3/layer-ciri.png";
 
 function App() {
     return (
@@ -31,27 +31,31 @@ function App() {
             parallax={true}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+            style={{ background: 'black' }}
         >
-            <SwiperSlide>
-                <div className="witcherBack" data-swiper-parallax="40%"/>
-                <div className="witcherMiddle" data-swiper-parallax="25%"/>
-                <div className="witcherFront" data-swiper-parallax="14%"/>
-                {/*<img src={witcherBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>*/}
-                {/*<img src={witcherMiddle} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>*/}
-                {/*<img src={witcherFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>*/}
+            <SwiperSlide style={{ height: '100vh', overflow: 'hidden' }}>
+                {/*<div style={{ height: '100vh', minWidth: '100vw' }} />*/}
+                {/*<div className="witcherBack" data-swiper-parallax="40%"/>*/}
+                {/*<div className="witcherMiddle" data-swiper-parallax="25%"/>*/}
+                {/*<div className="witcherFront" data-swiper-parallax="14%"/>*/}
+                <img src={witcherBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>
+                <img src={witcherMiddle} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>
+                <img src={witcherFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>
             </SwiperSlide>
-            {/*<SwiperSlide>*/}
-            {/*    <img src={geraBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>*/}
-            {/*    <img src={gera} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>*/}
-            {/*    <img src={yen} className="witcherMiddleOne" data-swiper-parallax="28%" alt="text"/>*/}
-            {/*    <img src={geraFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>*/}
-            {/*</SwiperSlide>*/}
-            {/*<SwiperSlide>*/}
-            {/*    <img src={ciriBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>*/}
-            {/*    <img src={ciriMiddle} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>*/}
-            {/*    <img src={ciri} className="witcherMiddleOne" data-swiper-parallax="28%" alt="text"/>*/}
-            {/*    <img src={ciriFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>*/}
-            {/*</SwiperSlide>*/}
+            <SwiperSlide style={{ height: '100vh', overflow: 'hidden' }}>
+                {/*<div style={{ height: '100vh', minWidth: '100vw' }} />*/}
+                <img src={geraBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>
+                <img src={gera} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>
+                <img src={yen} className="witcherMiddleOne" data-swiper-parallax="28%" alt="text"/>
+                <img src={geraFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>
+            </SwiperSlide>
+            <SwiperSlide style={{ height: '100vh', overflow: 'hidden' }}>
+                {/*<div style={{ height: '100vh', minWidth: '100vw' }} />*/}
+                <img src={ciriBack} className="witcherBackOne" data-swiper-parallax="40%" alt="text"/>
+                <img src={ciriMiddle} className="witcherMiddleOne" data-swiper-parallax="25%" alt="text"/>
+                <img src={ciri} className="witcherMiddleOne" data-swiper-parallax="28%" alt="text"/>
+                <img src={ciriFront} className="witcherFrontOne" data-swiper-parallax="14%" alt="text"/>
+            </SwiperSlide>
         </Swiper>
         // <Swiper
         //     // install Swiper modules
